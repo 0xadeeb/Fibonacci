c6d10f47b4c1edbb30675b6826cc7d047b1ddcf0
Adeeb HS <Adeeb.hs1@gmail.com>
2023-04-20 22:23:29 +0530

    add: comments

--- fib.s
+++ fib.s
@@ -7,24 +7,26 @@ fib_odd:
     push rbp
     mov rbp, rsp
 
-    cmp edi, 1
-    je base_case
+
+    cmp edi, 1          ;; Frist integer arg in EDI
+    je base_case        ;; n == 1 ? base_case
 
     mov ecx, edi
     sub ecx, 1
 
-    push rdi
+    push rdi            ;; Save context
 
-    mov edi, ecx
-    call fib_even
+    mov edi, ecx        ;; Arg 1 in EDI
+    call fib_even       ;; fib_even on n - 1, return value stored in EAX
 
-    pop rdi
-    mul edi
+    pop rdi             ;; Restore context
+
+    mul edi             ;; MUL -> EXD:EAX = EAX * EDI
 
     jmp _exit
 
 base_case:
-    mov eax, 1
+    mov eax, 1          ;; Return value in EAX
 
 _exit:
     pop rbp
